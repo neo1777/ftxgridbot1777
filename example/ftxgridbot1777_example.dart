0f39d001b8d47d9ec751601156d4ce33c684d5fa
neo1777 <banner@nicolainformatica.it>
2021-02-25 22:00:24 +0100

    delete start

--- example/ftxgridbot1777_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ftxgridbot1777/ftxgridbot1777.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/ftxgridbot1777.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library ftxgridbot1777;
-
-export 'src/ftxgridbot1777_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
